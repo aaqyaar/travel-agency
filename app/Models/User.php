bbec8a54459cfc551f1d4902d2648efd8900d89a
abdizamedmo <abdizamedmo@gmail.com>
2025-02-12 16:31:56 +0300

    refactor: remove unused HasFactory trait from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Filament\Panel;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -12,7 +9,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable, HasRoles;
+    use Notifiable, HasRoles;
 
 
     /**
